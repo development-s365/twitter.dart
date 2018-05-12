a0a0636a26569e1b63d3b57340aba1f32bb42b7f
programmeur <programmeur@vm-xubuntu.software365.local>
2018-05-12 15:06:02 +0200

    Final Commit

--- lib/twitter.dart
+++ lib/twitter.dart
@@ -1,41 +1,62 @@
 import 'dart:async';
-import 'dart:convert';
 
-import 'package:oauth2/oauth2.dart' as oauth;
-import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 
-import 'src/client.dart';
-import 'src/twitter_stream.dart';
 
 export 'src/twitter_stream.dart';
 
-final authorizationEndpoint =
-    Uri.parse("http://example.com/oauth2/authorization");
 final tokenEndpoint =
     Uri.parse("https://api.twitter.com/oauth2/token");
 
 /// A Class for Twitter
 class Twitter {
+  final String _bearer;
+  Completer<http.Response> _completer = new Completer<http.Response>.sync();
   /// Twitter API Endpoint
   final String baseUrl = 'https://api.twitter.com/1.1/';
-  final String CONSUMER_KEY = "YPMSDziqHsPPgFr5TAilswgqS";
-  final String CONSUMER_SECRET = "eeA09Wn22HJ9wFXeIs50IVUREltRaG2Gd956hd7KJjXd4DZnxs";
-  
-  Twitter(){
 
+  
+  Twitter(this._bearer){
+    /*
     var str = CONSUMER_KEY + ":" + CONSUMER_SECRET;
     var bytes = UTF8.encode(str);
     var secret = BASE64.encode(bytes);
 
-    // test
 
-    http.post(tokenEndpoint, body: {"grant-type" : "client_credentials"}, headers: {
+
+    http.post(tokenEndpoint, body: "grant_type=client_credentials", headers: {
       'Authorization' : 'Basic ' + secret,
       'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8'
     }).then((response){
       print("Response status: ${response.statusCode}");
       print("Response body: ${response.body}");
+      
+    });
+    
+    http.get(baseUrl + "statuses/user_timeline.json?screen_name=twitterapi",headers: {
+      'Authorization' : bearer
+    }).then((response){
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+
+    });*/
+  }
+
+  Future<http.Response> request(String endPoint) {
+    if(_completer.isCompleted) {
+      _completer = new Completer<http.Response>.sync();
+    }
+    var requestUrl = baseUrl + endPoint;
+    _request(requestUrl);
+    return _completer.future;
+  }
+
+  _request(String requestUrl) async{
+    http.get(requestUrl,headers: {
+      'Authorization' : _bearer
+    }).then((response){
+      print("Response status: ${response.statusCode}");
+      _completer.complete(response);
     });
   }
 }
