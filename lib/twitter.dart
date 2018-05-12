d1526f1ad95739f2633577838b9aa6e8a769cf5a
programmeur <programmeur@vm-xubuntu.software365.local>
2018-05-12 12:38:50 +0200

    twitter.dart edited

--- lib/twitter.dart
+++ lib/twitter.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:oauth/oauth.dart' as oauth;
+import 'package:oauth2/oauth2.dart' as oauth;
+import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 
 import 'src/client.dart';
@@ -8,90 +10,32 @@ import 'src/twitter_stream.dart';
 
 export 'src/twitter_stream.dart';
 
+final authorizationEndpoint =
+    Uri.parse("http://example.com/oauth2/authorization");
+final tokenEndpoint =
+    Uri.parse("https://api.twitter.com/oauth2/token");
+
 /// A Class for Twitter
 class Twitter {
   /// Twitter API Endpoint
   final String baseUrl = 'https://api.twitter.com/1.1/';
+  final String CONSUMER_KEY = "YPMSDziqHsPPgFr5TAilswgqS";
+  final String CONSUMER_SECRET = "eeA09Wn22HJ9wFXeIs50IVUREltRaG2Gd956hd7KJjXd4DZnxs";
+  
+  Twitter(){
 
-  /// oauth.Tokens for [Client]
-  oauth.Tokens oauthTokens;
+    var str = CONSUMER_KEY + ":" + CONSUMER_SECRET;
+    var bytes = UTF8.encode(str);
+    var secret = BASE64.encode(bytes);
 
-  /// An HTTP Client
-  Client twitterClient;
+    // test
 
-  Completer<http.Response> _completer = new Completer<http.Response>.sync();
-  int _counter = 0;
-
-  Twitter._internal(this.oauthTokens, this.twitterClient);
-
-  /// Creates a new [Twitter] instance from [consumerKey],[consumerSecret],[accessToken],[accessSecret]
-  factory Twitter(String consumerKey, String consumerSecret, String accessToken,
-      String accessSecret) {
-    oauth.Tokens oauthTokens = new oauth.Tokens(
-        consumerId: consumerKey,
-        consumerKey: consumerSecret,
-        userId: accessToken,
-        userKey: accessSecret);
-    Client twitterClient = new Client(oauthTokens);
-    return new Twitter._internal(oauthTokens, twitterClient);
-  }
-
-  /// Create a new [Twitter] instance from Map.
-  ///
-  /// ```dart
-  /// Map keymap = {"consumerKey" : "YOUR CONSUMER KEY",
-  ///               "consumerSecret" : "YOUR CONSUMER SECRET",
-  ///               "accessToken" : "YOUR ACCESS TOKEN",
-  ///               "accessSecret" : "YOUR ACCESS SECRET"};
-  /// Twitter twitter = new Twitter.fromMap(keyMap);
-  ///```
-  ///
-  factory Twitter.fromMap(Map keyMap) {
-    oauth.Tokens oauthTokens = new oauth.Tokens(
-        consumerId: keyMap['consumerKey'],
-        consumerKey: keyMap['consumerSecret'],
-        userId: keyMap['accessToken'],
-        userKey: keyMap['accessSecret']);
-    Client twitterClient = new Client(oauthTokens);
-    return new Twitter._internal(oauthTokens, twitterClient);
-  }
-
-  /// send a request to Twitter
-  ///
-  /// [method] is HTTP method name, for example "GET" , "POST".
-  /// [endPoint] is REST API Name of Twitter. for example "statuses/mentions_timeline.json".
-  /// [body] is HTTP Request's body.
-  Future<http.Response> request(String method, String endPoint, {Map body}) {
-    _counter++;
-    if (_completer.isCompleted) {
-      _completer = new Completer<http.Response>.sync();
-    }
-    var requestUrl = baseUrl + endPoint;
-    _request(method, requestUrl, body: body);
-    return _completer.future;
-  }
-
-  _request(String method, String requestUrl, {Map body}) async {
-    if(twitterClient.client == null){
-      twitterClient = new Client(oauthTokens);
-    }
-    var response = await twitterClient.request(method, requestUrl, body: body);
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      _completer.complete(response);
-    } else {
-      _completer.completeError(response.reasonPhrase);
-    }
-    _counter--;
-    if(_counter == 0){
-      twitterClient.close();
-    }
-  }
-
-  /// Connect to Twitter User Stream
-  Future<TwitterStream> getUserStream() async {
-    Uri uri = Uri.parse("https://userstream.twitter.com/1.1/user.json");
-    var request = new http.Request("GET", uri);
-    var response = await twitterClient.send(request);
-    return new TwitterStream(response.stream.toStringStream());
+    http.post(tokenEndpoint, headers: {
+      'Authorization' : 'Basic ' + secret,
+      'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8'
+    }, body: 'grant-type=client-credentials').then((response){
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+    });
   }
 }
